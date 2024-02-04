d9f6a52567f97804b9fdf63a058780395a47bfde
Ramram <ram93@gmail.com>
2024-02-04 21:59:32 +0530

    dropdownlist

--- src/components/DropDownList.js
+++ src/components/DropDownList.js
@@ -22,7 +22,7 @@ const DropDownList = ({dataSource}) => {
       {({ open }) => (
         <>
           <div className="relative mt-2">
-            <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm  ring-gray-300 focus:outline-none sm:text-sm sm:leading-6">
+            <Listbox.Button className="relative w-full cursor-default bg-white dark:bg-zinc-900 dark:text-white py-1.5 pl-3 pr-10 text-left text-gray-900 ring-gray-300 focus:outline-none sm:text-sm sm:leading-6">
               <span className="flex items-center">
                 {/* <img src={selected?.avatar || ''} alt="" className="h-5 w-5 flex-shrink-0 rounded-full" /> */}
                 <span className="ml-3 block truncate">{selected?.attributes?.name}</span>
@@ -39,7 +39,7 @@ const DropDownList = ({dataSource}) => {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Listbox.Options className="absolute z-10 mt-1 min-w-fit max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              <Listbox.Options className="absolute z-10 mt-1 min-w-fit max-h-56 w-full overflow-auto rounded-md bg-white dark:bg-zinc-900 dark:text-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                 {dataSource.map((item) => (
                   <Listbox.Option
                     key={item.id}
