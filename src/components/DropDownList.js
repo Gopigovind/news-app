5a5dddea88c892fd58fcde2fb70113032750a997
Ramram <ram93@gmail.com>
2024-02-04 21:54:38 +0530

    dropdownlist

--- src/components/DropDownList.js
+++ src/components/DropDownList.js
@@ -24,7 +24,7 @@ const DropDownList = ({dataSource}) => {
           <div className="relative mt-2">
             <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm  ring-gray-300 focus:outline-none sm:text-sm sm:leading-6">
               <span className="flex items-center">
-                <img src={selected?.avatar || ''} alt="" className="h-5 w-5 flex-shrink-0 rounded-full" />
+                {/* <img src={selected?.avatar || ''} alt="" className="h-5 w-5 flex-shrink-0 rounded-full" /> */}
                 <span className="ml-3 block truncate">{selected?.attributes?.name}</span>
               </span>
               <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
@@ -53,12 +53,17 @@ const DropDownList = ({dataSource}) => {
                   >
                     {({ selected, active }) => (
                       <>
-                        <div className="flex items-center">
-                          <img src={item.avatar || ''} alt="" className="h-5 w-5 flex-shrink-0 rounded-full" />
+                        <div className="flex items-center flex-col pr-6">
+                          {/* <img src={item.avatar || ''} alt="" className="h-5 w-5 flex-shrink-0 rounded-full" /> */}
                           <span
                             className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}
                           >
                             {item.attributes.name}
+                          </span>
+                          <span
+                            className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}
+                          >
+                            {item.attributes.localName}
                           </span>
                         </div>
 
