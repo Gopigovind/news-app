be8a9c068332013fef1c612aeceeb0db778c61fc
Ramram <ram93@gmail.com>
2024-02-29 21:46:50 +0530

    resolved state selection

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -32,7 +32,7 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { BASE_URL } from "./../utils/constants";
 import { toggleMenu } from "../utils/appSlice";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { isMobile as mobileDevice } from "../utils/helper";
 
 const SideBar = () => {
@@ -40,7 +40,8 @@ const SideBar = () => {
   const selector = useSelector((store) => store.newsCategory.category);
   const breakpoint = 1024;
   const dispatch = useDispatch();
-
+  const location = useLocation();
+  const [pathName, setPathName] = useState();
   useEffect(() => {
     const checkisMobile = () => {
       if (isMenuOpen && window.innerWidth < breakpoint) {
@@ -52,26 +53,40 @@ const SideBar = () => {
   }, []);
   const isMobile = () => window.innerWidth <= 1300;
 
-
   const [categoryGroup, setCategoryGroup] = useState([]);
-  const [newsCategory, setNewsCategory] = useState([]);
+  const [newsCategory, setNewsCategory] = useState();
   const localeName = useSelector((store) => store.app.locale);
 
   const getCategoryGroup = async () => {
-    const response = await fetch(`${BASE_URL}/category-groups?locale=${localeName}`);
+    const response = await fetch(`${BASE_URL}/category-groups?locale=${localeName}&populate=*&sort=id`);
     const { data } = await response.json();
     setCategoryGroup(data);
+    const urlName = location.pathname.split('/').length === 2 ? decodeURIComponent(location.pathname.split('/')[1]) : '';
+    listHandler(urlName, data);
   }
 
   const getNewsCategory = async () => {
-    const response = await fetch(`${BASE_URL}/news-categories?locale=${localeName}`);
+    const response = await fetch(`${BASE_URL}/news-categories?locale=${localeName}&sort=id`);
     const { data } = await response.json();
     setNewsCategory(data);
   }
 
+  const listHandler = (categoryName, data=null) => {
+    const itemData = data || categoryGroup;
+    const filterData = itemData.filter((item) => {
+      item.attributes.isActive = false;
+      return categoryName ? item.attributes.name === categoryName : item
+    });
+    if (filterData?.length > 0)
+    {
+      filterData[0].attributes.isActive = true;
+      setNewsCategory(filterData[0]);
+    }
+  }
+
   useEffect(() => {
     getCategoryGroup();
-    getNewsCategory();
+    // getNewsCategory();
   }, [localeName]);
 
   useEffect(() => {
@@ -92,32 +107,38 @@ const SideBar = () => {
     // dispatch(changeCategory({ type: 'CATEGORY', value: newsCat.id }));
   }
 
+
   return isMenuOpen ? (
     <>
-      <div className={`sidebar__open border-r dark:border-none flex ${isMobile() ? 'fixed top-0 z-30' : ''} flex-col w-[15rem] min-w-fit bg-white dark:bg-zinc-900 dark:text-white transition-all duration-500`}>
+      <div className={`sidebar__open border-r dark:border-none flex ${isMobile() ? 'fixed top-0 z-30' : ''} flex-col w-[15rem] min-w-fit bg-white dark:bg-zinc-900 dark:text-white transition-all duration-500`} style={{height: '100vh', overflowY: 'auto'}}>
         <div className="first-part flex pl-2 pr-6  pb-4 flex-col text-sm w-[15rem] ">
           {
             categoryGroup.length > 0 && categoryGroup.map((category, index) => (
-              <Link to="/" key={index} data-id={category.id} onClick={() => { dispatch(toggleMenu()); }}>
-                <div className="home px-4 flex py-2 items-center hover:bg-zinc-100 dark:hover:bg-zinc-700 w-full rounded-lg  cursor-pointer">
+              <Link to={category.attributes?.isDefault ? '/' : category?.attributes?.name} key={index} data-id={category.id} onClick={()=> listHandler(category?.attributes?.name)}>
+                <div className={`${((pathName === (category.attributes?.isDefault ? '/' : category?.attributes?.name)) || category?.attributes.isActive) ? 'bg-gray-500' : ''} home px-4 flex py-2 items-center hover:bg-zinc-100 dark:hover:bg-zinc-700 w-full rounded-lg  cursor-pointer`}>
                   {/* <MdHomeFilled size="1.5rem" className="mb-1 mr-4" /> */}
+                  {
+                    category.attributes?.image?.data?.attributes?.formats?.thumbnail?.url && (
+                      <img src={category.attributes?.image?.data?.attributes?.formats?.thumbnail?.url} className="mb-1 mr-4" style={{width: '1.5rem'}} />
+                    )
+                  }
                   <span className="">{category.attributes.name}</span>
                 </div>
               </Link>
             ))
           }
           {
-            newsCategory?.length > 0 && (
+            newsCategory && (
               <>
                 <div className="pt-3 border-b border-zinc-200 w-full"></div>
                 <div className="pt-4 pl-4 mb-2">
-                  <span className="text-base font-semibold">News</span>
+                  <span className="text-base font-semibold">{newsCategory?.attributes?.label}</span>
                 </div>
               </>
             )
           }
           {
-            newsCategory?.length > 0 && newsCategory.map((newsCat) => (
+            newsCategory?.attributes?.headline_categories?.data?.length > 0 && newsCategory?.attributes?.headline_categories?.data.map((newsCat) => (
               <Link
                 onClick={() => { dispatch(toggleMenu()); }}
                 state={{ type: 'CATEGORY', value: newsCat.id, item: newsCat }}
@@ -131,38 +152,10 @@ const SideBar = () => {
                   <span>{newsCat.attributes.name}</span>
                 </div>
               </Link>
+              
 
             ))
           }
-          {/* <div className="pt-3 border-b border-zinc-200  w-full"></div>
-          <div className="pt-4 pl-4">
-            <span className="text-sm">
-              Sign in to like videos, comment, and subscribe.
-            </span>
-          </div>
-          <div className="pt-4 pl-4">
-            <button className="border p-2  px-4 rounded-3xl  font-bold flex items-center gap-2">
-              <FaUserCircle size="1.5rem" /> Sign in
-            </button>
-          </div>
-          <div className="pt-3 border-b border-zinc-200 w-full mb-2 "></div>
-          <div className="links1 py-2 px-4  flex gap-1 flex-wrap font-medium text-[0.84rem] text-[#606060] dark:text-zinc-400">
-            <div className="cursor-pointer ml-1">About</div>
-            <div className="cursor-pointer ml-1">Press</div>
-            <div className="cursor-pointer">Copyright</div>
-            <div className="cursor-pointer ml-1">Contact us</div>
-            <div className="cursor-pointer ml-1">Creators</div>
-            <div className="cursor-pointer ml-1">Advertise</div>
-            <div className="cursor-pointer ml-1">Developers</div>
-          </div>
-          <div className="links1 py-2 px-4  flex gap-1 flex-wrap font-medium text-[0.84rem] text-[#606060] dark:text-zinc-400">
-            <div className="cursor-pointer ml-1">Terms</div>
-            <div className="cursor-pointer ml-1">Privacy</div>
-            <div className="cursor-pointer ml-1">Policy & Safety</div>
-            <div className="cursor-pointer ml-1">How Hyper Local News Updates works</div>
-            <div className="cursor-pointer ml-1">Test new features</div>
-          </div>
-          <div className="px-4 py-2 text-gray-400"> &copy; 2023 Google LLC</div> */}
         </div>
       </div>
       {
@@ -179,28 +172,21 @@ const SideBar = () => {
   ) : (
     !mobileDevice && (
       <div className="sidebar__closed pr-1 border-r dark:border-none flex flex-col text-xs w-18 items-center  h-[calc(100vh-4.625rem)] bg-white dark:bg-zinc-900 dark:text-white transition-all duration-500">
-        <Link to="/" className=" w-full">
-          <div className="home py-4 flex flex-col items-center hover:bg-zinc-200 dark:hover:bg-zinc-700  w-full rounded-md">
-            <MdHomeFilled size="1.5rem" className="mb-1" />
-            <span>Home</span>
-          </div>
-        </Link>
-        <div className="shorts py-4 flex flex-col items-center  hover:bg-zinc-200 dark:hover:bg-zinc-700  w-full rounded-md">
-          <img
-            src={shortsLogo_light_theme}
-            alt="shorts-logo"
-            className="w-10 mb-1"
-          />
-          <span>Shorts</span>
-        </div>
-        <div className="subscriptions py-4 flex flex-col items-center  hover:bg-zinc-200 dark:hover:bg-zinc-700 w-full rounded-md cursor-pointer">
-          <MdOutlineSubscriptions size="1.5rem" className="mb-1" />
-          <span>Subscriptions</span>
-        </div>
-        <div className="library py-4 flex flex-col items-center hover:bg-zinc-200 dark:hover:bg-zinc-700   w-full rounded-md cursor-pointer">
-          <MdOutlineVideoLibrary size="1.5rem" className="mb-1" />
-          <span>Library</span>
-        </div>
+        {
+            categoryGroup.length > 0 && categoryGroup.map((category, index) => (
+              <Link className=" w-full" to={category.attributes?.isDefault ? '/' : category?.attributes?.name} key={index} data-id={category.id} onClick={()=> listHandler(category?.attributes?.name)}>
+                <div className={`${((pathName === (category.attributes?.isDefault ? '/' : category?.attributes?.name)) || category?.attributes.isActive) ? 'bg-gray-500' : ''} home py-4 flex flex-col items-center hover:bg-zinc-200 dark:hover:bg-zinc-700  w-full rounded-md`}>
+                  {/* <MdHomeFilled size="1.5rem" className="mb-1 mr-4" /> */}
+                  {
+                    category.attributes?.image?.data?.attributes?.formats?.thumbnail?.url && (
+                      <img src={category.attributes?.image?.data?.attributes?.formats?.thumbnail?.url} className="mb-1 mr-4" style={{width: '1.5rem'}} />
+                    )
+                  }
+                  <span className="">{category.attributes.name}</span>
+                </div>
+              </Link>
+            ))
+          }
       </div>
     )
   );
