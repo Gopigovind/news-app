2edb335a61cf7b18eeb1330d0da550aed8e59027
Ramram <ram93@gmail.com>
2024-03-10 22:31:27 +0530

    resolved

--- src/components/BreadCrum.js
+++ src/components/BreadCrum.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { useLocation, Link, useParams, useNavigate } from 'react-router-dom';
-import { useSelector, } from "react-redux";
+import { useSelector, useDispatch} from "react-redux";
 import { BsHouseDownFill } from "react-icons/bs";
+import { updateDistrict, updateLocale, updateState, updateModal, updateTaluk } from "../utils/appSlice";
 
 const BreadCrum = () => {
 
@@ -12,6 +13,7 @@ const BreadCrum = () => {
   const districtName = useSelector((store) => store.app.districtName);
   const talukName = useSelector((store) => store.app.talukName);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   let {state= stateName, district=districtName, taluk=talukName, mainCategory='', newsCategory=''} = useParams();
     const pathList = () => {
         const pathItem = decodeURIComponent(location.pathname)?.split('/').filter(item => item && (item !== 'news'));
@@ -23,6 +25,12 @@ const BreadCrum = () => {
         width: "30%",
         overflow: 'hidden'
     };
+    const clickHandler = (path) => {
+        localStorage.setItem('district', path === district ? district : '');
+        dispatch(updateDistrict(path === district ? district : ''));
+        localStorage.setItem('taluk', path === taluk ? taluk : '');
+        dispatch(updateTaluk(path === taluk ? taluk : ''));
+    }
     return (
         <>
             {
@@ -41,7 +49,7 @@ const BreadCrum = () => {
                                 pathList()?.map((path, index) => (
                                     <li>
                                         <div className="flex items-center">
-                                            <Link to={`/${pathList().slice(0, index + 1).join('/')}`} className="text-sm hover:text-slate-600 sm:block" style={path.length > 50 ? textEllipse : {whiteSpace: "nowrap"}}>{path.replaceAll('%20', ' ')}</Link>
+                                            <Link onClick={() => clickHandler(path.replaceAll('%20', ' '))} to={`/${pathList().slice(0, index + 1).join('/')}`} className="text-sm hover:text-slate-600 sm:block" style={path.length > 50 ? textEllipse : {whiteSpace: "nowrap"}}>{path.replaceAll('%20', ' ')}</Link>
                                             {
                                                 (pathList().length - 1 !== index) ? (
                                                     <div aria-hidden="true" className="mx-2 select-none sm:block">/</div>
